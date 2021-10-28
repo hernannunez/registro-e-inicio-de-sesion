9b652ca2ab5c1246eeae4ccc8a99263acc906911
Hernan Javier Nuñez <89444404+hernannunez@users.noreply.github.com>
2021-10-28 03:13:59 -0300

    Create navbar.php

---
+++ navbar.php
@@ -0,0 +1,22 @@
+nav class="navbar" role="navigation">
+                <div class="container-fluid justify-content-md-center">
+                    <div class="row">
+                        <div class="col-12 p-4">
+                        <ul class="nav">
+                                <li class = "nav-item">
+                                    <a class="nav-link link-cust" href="index.php">Inicio</a>
+                                </li>
+                                <li class = "nav-item">
+                                    <a class="nav-link link-cust" href="ListaDePersonas.php">Lista de Personas</a>
+                                </li>
+                                <li class = "nav-item">
+                                    <a class="nav-link link-cust" href="secreto.php">Secreto</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link link-cust" href="armartarjeta.php">Armar tarjeta</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>

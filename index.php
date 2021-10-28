71c095cbb34e593a8b47276c9b086c0b2f18c551
Hernan Javier Nuñez <89444404+hernannunez@users.noreply.github.com>
2021-10-28 03:10:55 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilo.css"    
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body id="portada">
+    <div class="container-fluid">
+        <header>
+            <nav>
+            <?php include_once("navbar.php"); ?>
+            </nav>
+        </header>
+
+    </div>
+
+    <div class="container-fluid">
+        <main>
+            <div class="row">
+                <div class="col-12">
+                    <div class="container ms-7 p-5">
+                        <h1 class="text-cust"> Inicio<Hernan class=""></Hernan></h1>
+                        <p><button class="btn btn-dark">Ver listado de registros</button></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="container ms-7 p-5">
+                        <p class="sobremi">Lorem ipsum dolor sit amet consectetur adipiscing elit dis eleifend faucibus, taciti quisque urna nascetur<br>
+                            iaculis habitant penatibus pulvinar. Habitant ridiculus mattis morbi rutrum lacinia nulla class, ultrices tellus<br>
+                            pretium ullamcorper magnis consequat convallis eleifend, quam pulvinar placerat praesent iaculis massa.<br>
+                            Diam dictumst scelerisque pulvinar integer lectus pharetra congue ullamcorper, aptent sapien curae sociosqu<br>
+                            dis eu sem sodales, cubilia feugiat nam tempor iaculis torquent nibh.</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+        <footer>
+            <?php include_once("footer.php"); ?> 
+        </footer>
+    </div>
+    
+</body>
+</html>

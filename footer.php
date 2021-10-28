18b43addfe57ea4f647345775b54d6974f6f449f
Hernan Javier Nuñez <89444404+hernannunez@users.noreply.github.com>
2021-10-28 03:14:27 -0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,12 @@
+<footer class="navbar">
+            <div class="container-fluid justify-content-center align-items-center d-flex pt-2">
+                <div class="row">
+                    <div class="col-12 col-sm-6 p-5">
+                        <img src="imagenes/25231.png" href="#" width="45px" height="45px">
+                    </div>
+                    <div class="col-12 col-sm-6 p-5">
+                        <img src="imagenes/png-transparent-computer-icons-linkedin-social-media-social-media-text-rectangle-logo.png" href="#" width="45px" height="45px">
+                    </div>
+                </div>    
+            </div>
+        </footer>
